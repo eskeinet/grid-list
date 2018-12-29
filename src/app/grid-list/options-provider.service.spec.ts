3eef68fa6ca503c464b2d3c3db7301e8bad87b1f
eskei <eskei@eskei.net>
2018-12-29 15:46:22 +0000

    more column definitions tests and refinement

--- src/app/grid-list/options-provider.service.spec.ts
+++ src/app/grid-list/options-provider.service.spec.ts
@@ -3,6 +3,7 @@ import {TestBed} from '@angular/core/testing';
 import {OptionsProviderService} from './options-provider.service';
 import {GridListModule} from './grid-list.module';
 import {GridOptions} from 'ag-grid-community';
+import {ColDef} from 'ag-grid-community/dist/lib/entities/colDef';
 
 describe('OptionsProviderService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -17,17 +18,23 @@ describe('OptionsProviderService', () => {
   describe('| getOptions =>', () => {
     it('should return an object', () => {
       const service: OptionsProviderService = TestBed.get(OptionsProviderService);
-      expect(service.getOptions()).toEqual(jasmine.any(Object));
+      expect(service.getOptions([])).toEqual(jasmine.any(Object));
     });
 
     it('options should specify column definitions', () => {
       const service: OptionsProviderService = TestBed.get(OptionsProviderService);
-      const gridOptions: GridOptions = service.getOptions();
+      const colDefs = [
+        {headerName: 'Make', field: 'make'},
+        {headerName: 'Model', field: 'model'},
+        {headerName: 'Price', field: 'price'},
+      ];
+      const gridOptions: GridOptions = service.getOptions(colDefs);
       expect(gridOptions.columnDefs).toBeDefined();
       expect(gridOptions.columnDefs).toEqual(jasmine.any(Array));
       expect(gridOptions.columnDefs.length).toBeGreaterThan(0);
-      gridOptions.columnDefs.forEach(columnDef => {
+      gridOptions.columnDefs.forEach((columnDef: ColDef) => {
         expect(columnDef).toEqual(jasmine.any(Object));
+        expect(columnDef.headerName).toBeDefined();
       });
     });
   });
--- src/app/grid-list/options-provider.service.ts
+++ src/app/grid-list/options-provider.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {GridOptions} from 'ag-grid-community';
+import {ColDef} from 'ag-grid-community/dist/lib/entities/colDef';
 
 @Injectable()
 export class OptionsProviderService {
@@ -7,7 +8,9 @@ export class OptionsProviderService {
   constructor() {
   }
 
-  getOptions(): GridOptions {
-    return {};
+  getOptions(columnDefs: ColDef[]): GridOptions {
+    return {
+      columnDefs: columnDefs,
+    };
   }
 }
